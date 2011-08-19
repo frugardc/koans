3c062dc81e5fd31d2bfaa9169a7284c41c7628a7
Christian Frugard <christian.frugard@sidera.net>
2011-08-19 08:29:13 -0400

    finished about_objects

--- about_objects.rb
+++ about_objects.rb
@@ -51,7 +51,7 @@ class AboutObjects < EdgeCase::Koan
     obj = Object.new
     copy = obj.clone
 
-    assert_equal __, obj           != copy
-    assert_equal __, obj.object_id != copy.object_id
+    assert_equal true, obj           != copy
+    assert_equal true, obj.object_id != copy.object_id
   end
 end
